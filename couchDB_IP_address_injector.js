480fe5725639c1f759f5b3b71003c710bc957805
Navaneeth Rajagopalan <nrajagopalan@student.unimelb.edu.au>
2019-05-13 22:33:52 +1000

    Use middleware
    Signed-off-by: Navaneeth Rajagopalan <nrajagopalan@student.unimelb.edu.au>

--- couchDB_IP_address_injector.js
+++ couchDB_IP_address_injector.js
@@ -1,5 +1,5 @@
 fs = require('fs')
 
 couch_db_ip_address = process.argv[2];
-API_endPoint = 'http://' +  couch_db_ip_address + ':5984/ccc_a2/fc494be22f81a3914f7e621cde00067b'
+API_endPoint = 'http://' +  couch_db_ip_address + ':3001/api/anger-drug/co-relation'
 fs.writeFileSync(__dirname + '/src/assets/data/couch_API_URL.json', JSON.stringify({API_URL: API_endPoint}));
